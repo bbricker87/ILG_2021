ac1399449e2ff12b86ce65cd459aec0e15425bde
Ben Bricker <bbricker09@gmail.com>
2021-11-13 01:15:15 +0000

    Navbar & header

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,34 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-const sassdoc = require('sassdoc');
+const { series, src, dest } = require('gulp');
 
-var input = 'user/themes/fbc_2020/scss/**/*.scss';
-var output = 'user/themes/fbc_2020/css/';
+var input = './scss/**/*.scss';
+var output = './css/';
 var sassOptions = {
   errLogToConsole: true,
   outputStyle: 'expanded'
 };
-var autoprefixerOptions = {
-  browsers: ['last 2 versions', '>5%', 'Firefox ESR']
-};
-var sassdocOptions = {
-  dest: 'user/themes/fbc_2020'
-}
 
-gulp.task('sass', function () {
-  return gulp
-    .src(input)
+function compileSass() {
+  return src(input)
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions)).on('error', sass.logError)
     .pipe(sourcemaps.write())
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(gulp.dest(output))
+    .pipe(autoprefixer())
+    .pipe(dest(output))
   // .pipe(sassdoc(sassdocOptions))
   // .resume()
-});
-
-//Default task
-gulp.task('default', ['sass', 'watch']);
+};
 
 //Watch task
 function watch() {
-  watch(input, sass())
+  watch(input, compileSass)
     .on('change', function (event) {
       console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     });
 }
 
-// Production Build
-gulp.task('prod', function () {
-  return gulp
-    .src(input)
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(gulp.dest(output));
-});
-
-exports.watch =
+exports.build = compileSass;
+exports.default = series(compileSass, watch);
